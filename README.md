39551c060f63edd168b2ec618fb2e92b45178e6f
Kazumasa Utashiro <kaz@utashiro.com>
2020-07-10 21:31:33 +0900

    minil new Text::ANSI::Fold::Util

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::ANSI::Fold::Util - It's new $module
+
+# SYNOPSIS
+
+    use Text::ANSI::Fold::Util;
+
+# DESCRIPTION
+
+Text::ANSI::Fold::Util is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/Text/ANSI/Fold/Util.pm
@@ -0,0 +1,39 @@
+package Text::ANSI::Fold::Util;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::ANSI::Fold::Util - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::ANSI::Fold::Util;
+
+=head1 DESCRIPTION
+
+Text::ANSI::Fold::Util is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
